5f8a89ab9c829ddb74f823f7676f08c09200c55b
ANVITHA-KATIPALLY <99191579+ANVITHA-KATIPALLY@users.noreply.github.com>
2022-11-27 17:57:37 +0530

    Create frame.c

---
+++ FRAMES/frame/frame.c
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Experiment-1</title>
+</head>
+<frameset rows="23%,62%,15%">
+<frameset cols="100%">
+<frame name="frame1" src="frame1.html">
+</frameset>
+<frameset cols="20%,80%">
+<frame name="frame2" src="frame2.html">
+<frame name="frame3" src="frame3.html">
+</frameset>
+<frameset rows="100%">
+<frame name="frame4" src="frame4.html">
+</frameset>
+</frameset>
+
+</html>
